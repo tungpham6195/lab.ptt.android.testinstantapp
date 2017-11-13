cbcb10b63f18da86c61e8e551c764fe6ec253207
tungpham6195 <phamthanhtung1995@gmail.com>
2017-11-13 23:55:06 +0700

    start activity from base_module

---
+++ base/src/main/java/com/app/ptt/instantapp/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.app.ptt.instantapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- feature_instant/src/main/java/com/app/ptt/instantapp/feature_instant/MainActivity.java
+++ feature_instant/src/main/java/com/app/ptt/instantapp/feature_instant/MainActivity.java
@@ -1,7 +1,12 @@
 package com.app.ptt.instantapp.feature_instant;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.app.ptt.instantapp.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_loginFb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar.make(findViewById(R.id.btn_loginFb), getString(R.string.login_with_facebook), Snackbar.LENGTH_SHORT);
+                snackbar.show();
+                Intent i_facebook=new Intent(MainActivity.this, LoginActivity.class);
+                i_facebook.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i_facebook);
+            }
+        });
+        findViewById(R.id.btn_loginGM).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar.make(findViewById(R.id.btn_loginGM), getString(R.string.login_with_gmail), Snackbar.LENGTH_SHORT);
+                snackbar.show();
+                Intent i_gmail=new Intent(MainActivity.this, LoginActivity.class);
+                i_gmail.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i_gmail);
+            }
+        });
     }
 }
